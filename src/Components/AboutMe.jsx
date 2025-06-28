827a0396b9a24efdd8191c367d0d351c85ccbd23
HUSAIN <husain.hasan2011@gmail.com>
2025-06-28 16:21:29 +0700

    Foto

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,4 +1,3 @@
-import PF from "../assets/PF.jpg";
 import {motion} from "framer-motion";
 
 function AboutMe(){
@@ -11,7 +10,7 @@ function AboutMe(){
     >
         <section id="aboutme" className="py-16 px-6 text-center">
             <h2 className="text-3xl font-bold text-gray-600 mb-6">Tentang Saya</h2>
-            <img src={PF} alt="Foto Profil" className="mx-auto w-32 rounded-md object-cover mb-4 shadow-lg" />
+            <img src="src/assets/PF.JPG" alt="Foto Profil" className="mx-auto w-32 rounded-md object-cover mb-4 shadow-lg" />
             <p className="text-gray-700 leading-relaxed max-w-3xl mx-auto">
                 Halo! Saya <span className="font-semibold text-blue-600">Hasan</span> seorang mahasiswa Sistem Informasi yang sedang belajar React dan Tailwind. Saya sangat antusias untuk menjadi seorang Web Engineer yang bisa membuat website keren dan bermanfaat.
             </p>
