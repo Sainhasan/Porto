16716a4948a637a5eddfe323d3e01a8866acf017
HUSAIN <husain.hasan2011@gmail.com>
2025-06-30 15:58:57 +0700

    V.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <ToastContainer position="top-center" />
-      <div className="bg-gray-100 text-gray-800">
-        <AnimatedRoutes />
+      <div className="min-h-dvh bg-gray-100 text-gray-800 flex flex-col">
+        <Navbar />
+        <ToastContainer position="top-center" />
+        <div className="flex-1">
+          <AnimatedRoutes />
+        </div>
         <Footer />
       </div>
     </BrowserRouter>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 function Footer() {
   return (
     <motion.footer
-      className="bg-red-600 text-gray-600 text-center py-6 shadow-inner"
+      className="bg-white text-gray-600 text-center py-6 shadow-inner"
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
